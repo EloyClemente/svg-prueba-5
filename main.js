0fb0039d132593cdae8fb9a8d0b3b5ae8b482aeb
Eloy Clemente <eloyclemente1@gmail.com>
2021-12-08 20:32:11 +0100

    Terminados multiples yoes activos

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
 const snap = Snap('#SVG')
 const yo_central = snap.select('#circulo_pequeno')
 const yo_central_gradient = snap.select('#radial_gradient_yo_central')
+const yo_azul = snap.select('#feGaussianBlur41708')
 
 
-const yo_1_circulo = snap.select('#yo_1_circulo')
-const yo_1_brillo = snap.select('#yo_1_brillo')
-
-const radialGradient33487 = snap.select('#radialGradient33487')
-const stop_yo_1 = snap.select('#stop_yo_1')
-
-
-yo_1_circulo.mouseover(function() {
-    // yo_central.addClass('miclase')
-    // yo_central.animate({ r: 0 }, 100)
-    // yo_1_circulo.animate({ r: 400, fill: '#F1DBCB' }, 1000 ) 
-})
-
-yo_1_circulo.mouseout(function() {
-    // yo_central.removeClass('miclase')
-    // yo_central.animate({ r: 60 }, 1000)
-    // yo_1_circulo.animate({ r: 51.694, fill: 0 }, 1000 ) 
-})
+yo_azul.animate({
+    // stdDeviation: '10.468147 1 10.468147'
+}, 1000)
 
 
 
@@ -36,4 +22,7 @@ yo_central.mouseout(function() {
 })
 
 
+setTimeout(function(){
+
+}, 5000)
 
